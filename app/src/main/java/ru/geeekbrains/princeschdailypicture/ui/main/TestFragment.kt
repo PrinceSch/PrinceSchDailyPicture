5b1fba2868d3a915853774971d849b9d0abc4802
PrinceSch <sofy95@mail.ru>
2021-09-26 23:53:53 +0700

    потыкалась в Layout Behaviors, даже смогла вписать свой параметр :)

--- app/src/main/java/ru/geeekbrains/princeschdailypicture/ui/main/TestFragment.kt
+++ app/src/main/java/ru/geeekbrains/princeschdailypicture/ui/main/TestFragment.kt
@@ -1,25 +1,30 @@
 package ru.geeekbrains.princeschdailypicture.ui.main
 
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.fragment.app.Fragment
+import com.google.android.material.appbar.AppBarLayout
 import ru.geeekbrains.princeschdailypicture.databinding.FragmentTestBinding
 
-class TestFragment: Fragment() {
+class TestFragment : Fragment() {
 
     var _binding: FragmentTestBinding? = null
     val binding: FragmentTestBinding
         get() {
             return _binding!!
         }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding =  FragmentTestBinding.inflate(inflater)
-        return  binding.root
+        _binding = FragmentTestBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -34,5 +39,30 @@ class TestFragment: Fragment() {
 
     companion object {
         fun newInstance() = TestFragment()
+    }
+}
+
+class ViewAppBarBehavior @JvmOverloads constructor(
+    context: Context? = null, attrs: AttributeSet? = null
+) : CoordinatorLayout.Behavior<View>(context, attrs) {
+    override fun layoutDependsOn(
+        parent: CoordinatorLayout,
+        child: View,
+        dependency: View
+    ) = dependency is AppBarLayout
+
+    override fun onDependentViewChanged(
+        parent: CoordinatorLayout,
+        child: View,
+        dependency: View
+    ): Boolean {
+        val appBar = dependency as AppBarLayout
+        val currentAppBarHeight = appBar.height + appBar.y
+        val parentHeight = parent.height
+        val placeHolderHeight = (parentHeight - currentAppBarHeight).toInt()
+        appBar.alpha = (currentAppBarHeight/appBar.height)
+        child.layoutParams?.height = placeHolderHeight
+        child.requestLayout()
+        return false
     }
 }
